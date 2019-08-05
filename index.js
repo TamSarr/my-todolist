2bbe6d7b92d04ef37da10c247ce564ecb94febfc
himic <tamsar1111@gmail.com>
2019-08-05 18:45:09 +0300

    ADD: login on server start

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ app.use('/', express.static(__dirname + '/docs'));
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
+
+console.log( 'Server successfully started on http://localhost:5000' );
