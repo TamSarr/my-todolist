188509e696e86fea9cbb4b33e2b8ef091d406104
himic <tamsar1111@gmail.com>
2019-08-05 19:06:11 +0300

    UPD: project

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,21 @@
 'use strict';
-console.log( 'Hello' );
+( function() {
+	var list = [],
+		listElement = document.querySelector( '#list' ),
+		inputElement = document.querySelector( '#newTask' ),
+		addButtonElement = document.querySelector( '#addButton' );
+
+	addButtonElement.addEventListener( 'click', function() {
+		var value = inputElement.value;
+
+		if ( value.length < 1 ) {
+			return;
+		}
+
+		list.push( value );
+		console.log( list );
+
+		value = '';
+		console.log( value );
+	} );
+} )();
