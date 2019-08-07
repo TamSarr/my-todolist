c057142bdfedb8f1b51be7d0748459385d044ac1
himic <tamsar1111@gmail.com>
2019-08-07 19:09:59 +0300

    ADD: renderingList, removeItem, addItemInDom

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,11 @@
 'use strict';
 ( function() {
-	var list = [],
+	var list = [ 'one', 'two', 'three'],
 		listElement = document.querySelector( '#list' ),
 		inputElement = document.querySelector( '#newTask' ),
 		addButtonElement = document.querySelector( '#addButton' );
 
-	addButtonElement.addEventListener( 'click', function() {
+	function addItem() {
 		var value = inputElement.value;
 
 		if ( value.length < 1 ) {
@@ -15,7 +15,49 @@
 		list.push( value );
 		console.log( list );
 
-		value = '';
-		console.log( value );
-	} );
+		renderList();
+
+		inputElement.value = '';
+	};
+
+	function addItemInDom( item, index ) {
+		var newItem = document.createElement( 'li' );
+		newItem.innerHTML = item;
+
+		var removeButton = document.createElement( 'button' );
+		removeButton.innerHTML = 'x';
+		newItem.append( removeButton );
+
+		removeButton.addEventListener( 'click', function() {
+			removeItem( index );
+		} );
+
+		listElement.append( newItem );
+		newItem.setAttribute( 'data-item', index );
+
+		saveToLocalStorage()
+	};
+
+	function removeItem( index ) {
+		list.splice( index, 1 );
+		renderList();
+
+		saveToLocalStorage()
+	};
+
+	function saveToLocalStorage() {
+		console.log( JSON.stringify( list ) );
+		localStorage.setItem( 'tamsarr_', JSON.stringify( list ) );
+	}
+
+	function renderList() {
+		listElement.innerHTML = '';
+		list.forEach( function( item, index ) {
+			addItemInDom( item, index );
+		} );
+	};
+
+	addButtonElement.addEventListener( 'click', addItem );
+
+	renderList();
 } )();
